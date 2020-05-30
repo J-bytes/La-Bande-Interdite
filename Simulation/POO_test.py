d25932993aea0f249383a9cdfd3a5da17e6c6528
winteragon <olivier.denis.1@umontreal.ca>
2020-05-30 11:02:43 -0400

    developpement

--- Simulation/POO_test.py
+++ Simulation/POO_test.py
@@ -1,6 +1,12 @@
 class Person:
 
-    def __init__(self, x, y, status='healthy'):
+    def __init__(self, x, y, status='healthy', age, hygiene):
         self.x = x
         self.y = y
         self.status = status
+        self.age = age
+        self.hygiene = hygiene
+
+    def change_status(self,newstatus):
+        self.status = newstatus
+    
